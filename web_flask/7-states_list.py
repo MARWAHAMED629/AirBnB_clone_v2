b7a4570a63eb38f544dcb7baf75f77b6adc728d6
MARWAHAMED629 <mmero385@gmail.com>
2024-03-27 19:56:36 -0700

    7-states_list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application
 """
-from flask import Flask
-from flask import render_template
-from models import storage
-from models.state import State
-from models import storage
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Close the current SQLAlchemy session."""
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page with a list of state objects."""
-    states = storage.all('State').values()  # Fetch all State objects
-    sorted_states = sorted(states, key=lambda state: state.name)  # Sort by name
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
-    return render_template('states_list.html', states=sorted_states)
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
